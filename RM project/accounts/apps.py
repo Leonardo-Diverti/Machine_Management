cce1d885da78a9bea63f31c733e6cd0200b1b250
Leonardo Diverti <160747321+Leonardo-Diverti@users.noreply.github.com>
2026-07-24 14:34:08 +0200

    Delete RM project directory

--- RM project/accounts/apps.py
+++
@@ -1,7 +0,0 @@
-# Questo file configura l'app accounts all'interno di Django.
-from django.apps import AppConfig
-
-
-class AccountsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'accounts'
